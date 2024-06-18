c24b755de8ff4b08c0c0893e8b1725d5c527fa07
Dominik Rasiński <dox.gmr@gmail.com>
2024-06-18 22:39:26 +0200

    add images

--- stats-preview/src/components/column.tsx
+++ stats-preview/src/components/column.tsx
@@ -2,9 +2,11 @@ import { HeroImage } from './heroImage';
 import { Description } from './description';
 
 export const Column = () => {
+  const imageUrl = require('./assets/image-header-desktop.jpg');
+
   return (
     <div className='flex flex-col justify-center items-center bg-dark-desaturated-blue'>
-      <HeroImage image={''} />
+      <HeroImage image={imageUrl} />
       <Description />
     </div>
   );
